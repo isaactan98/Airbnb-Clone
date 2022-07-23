3f08cac49b4cbfc48832cfae87d40892bd9d0dbb
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-07-24 01:12:45 +0800

    update: user - top filter sticky

---
+++ airbnb-clone-user/components/TopFilter.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="sticky top-20 w-full pt-5 z-10 bg-white">
+        <div class="contents">
+            <div class="grid px-5 md:px-12 lg:px-16 xl:px-20">
+                <div class="flex h-[78px] items-center overflow-x-auto">
+                    <div class="relative grid gap-8 grid-flow-col justify-start">
+                        <div class="grid grid-cols-[100%]" v-for="x in 10" :key="x">
+                            <div class="">
+                                <button aria-hidden="false" class="text-center pt-1 pb-3" role="radio"
+                                    aria-checked="true" type="button">
+                                    <span class="flex flex-col items-center">
+                                        <img class=""
+                                            src="https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg"
+                                            alt="" width="24" height="24">
+                                        <div class="whitespace-nowrap relative">
+                                            <span class="inline-block text-xs">
+                                                Islands
+                                            </span>
+                                        </div>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="grid grid-cols-[100%]" v-for="x in 10" :key="x">
+                            <div class="">
+                                <button aria-hidden="false" class="text-center pt-1 pb-3" role="radio"
+                                    aria-checked="true" type="button">
+                                    <span class="flex flex-col items-center">
+                                        <img class="i1w8ohg7 dir dir-ltr"
+                                            src="https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg"
+                                            alt="" width="24" height="24">
+                                        <div class="whitespace-nowrap relative">
+                                            <span class="inline-block text-xs">
+                                                Amazing Pool
+                                            </span>
+                                        </div>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- airbnb-clone-user/layouts/default.vue
+++ airbnb-clone-user/layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <Header></Header>
+    <TopFilter></TopFilter>
     <Nuxt />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header.vue';
-export default { components: { Header } }
+import TopFilter from '../components/TopFilter.vue';
+export default { components: { Header, TopFilter } }
 </script>
 
 <style>
