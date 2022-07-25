828d511d62a2ea1cc88e610db56fa02ed0587b8a
Isaac Tan <73921980+isaactan98@users.noreply.github.com>
2022-07-26 00:02:41 +0800

    update: user - room detail img header

--- airbnb-clone-user/pages/rooms/_room.vue
+++ airbnb-clone-user/pages/rooms/_room.vue
@@ -11,15 +11,107 @@
           </div>
         </section>
       </div>
-      <div class="flex max-w-[1120px] mx-auto items-stretch w-full" v-else>
-        <section>
-          <h1 class="break-words text-2xl font-bold">{{ room_data.name }}, {{ room_data.address.cityName }}</h1>
-          <div class="flex justify-between items-end">
-            <span class="mt-1">
-              <a href="" class="text-sm underline text-black">{{ room_data.address.countryName }}</a>
-            </span>
+      <div v-else>
+        <div class="flex max-w-[1120px] mx-auto items-stretch w-full">
+          <section>
+            <h1 class="break-words text-2xl font-bold">{{ room_data.name }}, {{ room_data.address.cityName }}</h1>
+            <div class="flex justify-between items-end">
+              <span class="mt-1">
+                <a href="" class="text-sm underline text-black">{{ room_data.address.countryName }}</a>
+              </span>
+            </div>
+          </section>
+        </div>
+        <div class="pt-6 max-w-[1120px] mx-auto">
+          <div class="block">
+            <div class="rounded-3xl overflow-y-hidden">
+              <div class=" cal60_64px relative overflow-y-hidden">
+                <div class="h-0 min-h-full min-w-full relative pt-[50%]">
+                  <div class="absolute top-0 left-0 w-full h-full">
+                    <!-- 1st Col -->
+                    <div class="h-full w-1/2 absolute top-0 left-0">
+                      <div class=" cal60_64px h-full w-full min-h-[300px]">
+                        <div class="relative h-full w-full">
+                          <button class="h-full w-full relative text-left block">
+                            <div class="inline-block align-bottom h-full w-ful bg_50_50 bg-no-repeat ">
+                              <picture>
+                                <img
+                                  src="https://a0.muscache.com/im/pictures/373443ec-b377-4181-b753-3a2f3508c2b3.jpg?im_w=960"
+                                  alt="" class=" h-full w-full absolute object-cover align-bottom">
+                              </picture>
+                            </div>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- 2nd Col -->
+                    <div class="flex h-full w-1/4 pl-2 left-1/2 top-0 absolute flex-col">
+                      <div class="cal60_64px h-full w-full">
+                        <div class="h-full sm:h-1/2">
+                          <div class="relative h-full w-full">
+                            <button class=" h-full w-full relative text-left">
+                              <div class="inline-block align-bottom h-full w-ful bg_50_50 bg-no-repeat ">
+                                <picture>
+                                  <img
+                                    src="https://a0.muscache.com/im/pictures/7c586cfa-6a5c-4ec1-8fcd-5890b6a50769.jpg?im_w=720"
+                                    alt="" class=" object-cover align-bottom absolute h-full w-full">
+                                </picture>
+                              </div>
+                            </button>
+                          </div>
+                        </div>
+                        <div class="h-full sm:h-1/2 mt-2">
+                          <div class="relative h-full w-full">
+                            <button class=" h-full w-full relative text-left">
+                              <div class="inline-block align-bottom h-full w-ful bg_50_50 bg-no-repeat ">
+                                <picture>
+                                  <img
+                                    src="https://a0.muscache.com/im/pictures/7c586cfa-6a5c-4ec1-8fcd-5890b6a50769.jpg?im_w=720"
+                                    alt="" class=" object-cover align-bottom absolute h-full w-full">
+                                </picture>
+                              </div>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- 3rd Col -->
+                    <div class="flex h-full w-1/4 pl-2 right-0 top-0 absolute flex-col">
+                      <div class="cal60_64px h-full w-full">
+                        <div class="h-full sm:h-1/2">
+                          <div class="relative h-full w-full">
+                            <button class=" h-full w-full relative text-left">
+                              <div class="inline-block align-bottom h-full w-ful bg_50_50 bg-no-repeat ">
+                                <picture>
+                                  <img
+                                    src="https://a0.muscache.com/im/pictures/7c586cfa-6a5c-4ec1-8fcd-5890b6a50769.jpg?im_w=720"
+                                    alt="" class=" object-cover align-bottom absolute h-full w-full">
+                                </picture>
+                              </div>
+                            </button>
+                          </div>
+                        </div>
+                        <div class="h-full sm:h-1/2 mt-2">
+                          <div class="relative h-full w-full">
+                            <button class=" h-full w-full relative text-left">
+                              <div class="inline-block align-bottom h-full w-ful bg_50_50 bg-no-repeat ">
+                                <picture>
+                                  <img
+                                    src="https://a0.muscache.com/im/pictures/7c586cfa-6a5c-4ec1-8fcd-5890b6a50769.jpg?im_w=720"
+                                    alt="" class=" object-cover align-bottom absolute h-full w-full">
+                                </picture>
+                              </div>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </section>
+        </div>
       </div>
     </div>
   </main>
